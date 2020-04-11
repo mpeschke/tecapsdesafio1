60c9f928d2c1def14d3ae1f1d586c78ed295e31a
Matheus Peschke <2604242+mpeschke@users.noreply.github.com>
2020-04-11 17:46:52 -0300

    Testes unitários movidos para execução via CMake testing()

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
-# APS de Técnicas de Programação
+# APS de Técnicas de Programação - Desafio 1
 
-3 desafios da APS do professor Igor, 2º semestre de Ciência da Computação, 1º semestre de 2020, TÉCNICAS DE PROGRAMAÇÃO (P2N) - 202010.01041.01
+Primeiro desafio da APS do professor Igor, 2º semestre de Ciência da Computação, 1º semestre de 2020, TÉCNICAS DE PROGRAMAÇÃO (P2N) - 202010.01041.01
 
-## Dependências
+Enunciado do desafio 1 descrito no arquivo tp_ListaDesafiosAPS.pdf.
+
+## Passo 1: Dependências
 
 Linux (qualquer distro)
-CMake e GCC(ou CLang)
+Git >= 2.17.1, CMake >= 3.5 e GCC >= 7.5.0
 
 Por exemplo, no Ubuntu:  
 *#apt install -y git cmake make gcc*
 
-## Instruções para executar o Desafio 1
+## Passo 2: Instruções para executar o Desafio 1
 
-Baixe o repositório:
+Num terminal:
 
-*git clone ssh://git@github.com/mpeschke/tecdesafiosaps.git* (via ssh) ou
-*git clone https://github.com/mpeschke/tecdesafiosaps.git* (pergunta usuário e senha)
+*$git clone ssh://git@github.com/mpeschke/tecdesafiosaps.git* (via ssh) ou
+*%git clone https://github.com/mpeschke/tecdesafiosaps.git* (pergunta usuário e senha)
+*$cd tecapsdesafio1/*  
+*$mkdir tecapsdesafio1_build*  
+*$cd tecapsdesafio1_build/*  
+*$cmake ../.*  
+*$cmake --build .*  
+*$./desafio1*
 
-*cd tecdesafiosaps/*
+## Passo 3: Instruções para executar os testes unitários do Desafio 1
 
-*cmake CMakeLists.txt*
+Execute o passo 2.
 
-*make*
+A partir da linha de comando, execute:
 
-*./desafio1*
-
-(Vai executar somente os testes unitários do analisador sintático por enquanto - em desenvolvimento).
-
+*$ctest*
--- test/tests_desafio1.c
+++ test/tests_desafio1.c
@@ -1,206 +1,90 @@
-#include <stdio.h>
-#include "desafio1.h"
 #include "tests_desafio1.h"
 
-void test_unittests_desafio_1(void)
+void test_unittests_desafio_1(const int lexicalanalyser, const char* sentence)
 {
-    int testcounter = 0;
-    int failed = 0;
-    int passed = 0;
+    stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
+    stQuery qry = { .fn = {'\0'}, .ln = {'\0'}, .bd = {'\0'}, .pn = {'\0'}};
 
-    // Testa cenários para os quais o analisador sintático 'addcommandlexicalanalyser' deve
-    // validar a sentença como CORRETA (TRUE).
-    for(int i = 0; i < 12; i++)
+    switch(lexicalanalyser)
     {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = addcommandlexicalanalyser(add_syntaticanalyser_valid_testcases[i], &individuo);
-        printf("%s: Valor esperado: TRUE. Valor retornado: %s. Analisador sintático 'add' para a sentença '%s'.\n",
-               ret == TRUE ? "PASSED" : "FAILED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               add_syntaticanalyser_valid_testcases[i]);
-        if(ret)
-            passed++;
-        else
-            failed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'delcommandlexicalanalyser' deve
-    // validar a sentença como CORRETA (TRUE).
-    printf("\n");
-    for(int i = 0; i < 15; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = delcommandlexicalanalyser(del_syntaticanalyser_valid_testcases[i], &individuo);
-        printf("%s: Valor esperado: TRUE. Valor retornado: %s. Analisador sintático 'del' para a sentença '%s'.\n",
-               ret == TRUE ? "PASSED" : "FAILED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               del_syntaticanalyser_valid_testcases[i]);
-        if(ret)
-            passed++;
-        else
-            failed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'infocommandlexicalanalyser' deve
-    // validar a sentença como CORRETA (TRUE).
-    printf("\n");
-    for(int i = 0; i < 15; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = infocommandlexicalanalyser(info_syntaticanalyser_valid_testcases[i], &individuo);
-        printf("%s: Valor esperado: TRUE. Valor retornado: %s. Analisador sintático 'info' para a sentença '%s'.\n",
-               ret == TRUE ? "PASSED" : "FAILED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               info_syntaticanalyser_valid_testcases[i]);
-        if(ret)
-            passed++;
-        else
-            failed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'querycommandlexicalanalyser' deve
-    // validar a sentença como CORRETA (TRUE).
-    printf("\n");
-    for(int i = 0; i < 58; i++)
-    {
-        stQuery qry = { .fn = {'\0'}, .ln = {'\0'}, .bd = {'\0'}, .pn = {'\0'}};
-        BOOL ret = querycommandlexicalanalyser(query_syntaticanalyser_valid_testcases[i], &qry);
-        printf("%s: Valor esperado: TRUE. Valor retornado: %s. Analisador sintático 'query' para a sentença '%s'.\n",
-               ret == TRUE ? "PASSED" : "FAILED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               query_syntaticanalyser_valid_testcases[i]);
-        if(ret)
-            passed++;
-        else
-            failed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'terminatecommandlexicalanalyser' deve
-    // validar a sentença como CORRETA (TRUE).
-    printf("\n");
-    for(int i = 0; i < 5; i++)
-    {
-        BOOL ret = terminatecommandlexicalanalyser(terminate_syntaticanalyser_valid_testcases[i]);
-        printf("%s: Valor esperado: TRUE. Valor retornado: %s. Analisador sintático 'terminate' para a sentença '%s'.\n",
-               ret == TRUE ? "PASSED" : "FAILED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               terminate_syntaticanalyser_valid_testcases[i]);
-        if(ret)
-            passed++;
-        else
-            failed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'addcommandlexicalanalyser' deve
-    // validar a sentença como INCORRETA (FALSE).
-    printf("\n");
-    for(int i = 0; i < 27; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = addcommandlexicalanalyser(add_syntaticanalyser_invalid_testcases[i], &individuo);
-        printf("%s: Valor esperado: FALSE. Valor retornado: %s. Analisador sintático 'add' para a sentença '%s'.\n",
-               ret == TRUE ? "FAILED" : "PASSED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               add_syntaticanalyser_invalid_testcases[i]);
-        if(ret)
-            failed++;
-        else
-            passed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'delcommandlexicalanalyser' deve
-    // validar a sentença como INCORRETA (FALSE).
-    printf("\n");
-    for(int i = 0; i < 25; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = delcommandlexicalanalyser(del_syntaticanalyser_invalid_testcases[i], &individuo);
-        printf("%s: Valor esperado: FALSE. Valor retornado: %s. Analisador sintático 'del' para a sentença '%s'.\n",
-               ret == TRUE ? "FAILED" : "PASSED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               del_syntaticanalyser_invalid_testcases[i]);
-        if(ret)
-            failed++;
-        else
-            passed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'infocommandlexicalanalyser' deve
-    // validar a sentença como INCORRETA (FALSE).
-    printf("\n");
-    for(int i = 0; i < 34; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        BOOL ret = infocommandlexicalanalyser(info_syntaticanalyser_invalid_testcases[i], &individuo);
-        printf("%s: Valor esperado: FALSE. Valor retornado: %s. Analisador sintático 'info' para a sentença '%s'.\n",
-               ret == TRUE ? "FAILED" : "PASSED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               info_syntaticanalyser_invalid_testcases[i]);
-        if(ret)
-            failed++;
-        else
-            passed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'querycommandlexicalanalyser' deve
-    // validar a sentença como INCORRETA (FALSE).
-    printf("\n");
-    for(int i = 0; i < 69; i++)
-    {
-        stQuery qry = { .fn = {'\0'}, .ln = {'\0'}, .bd = {'\0'}, .pn = {'\0'}};
-        BOOL ret = querycommandlexicalanalyser(query_syntaticanalyser_invalid_testcases[i], &qry);
-        printf("%s: Valor esperado: FALSE. Valor retornado: %s. Analisador sintático 'query' para a sentença '%s'.\n",
-               ret == TRUE ? "FAILED" : "PASSED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               query_syntaticanalyser_invalid_testcases[i]);
-        if(ret)
-            failed++;
-        else
-            passed++;
-        testcounter++;
-    }
-
-    // Testa cenários para os quais o analisador sintático 'terminatecommandlexicalanalyser' deve
-    // validar a sentença como INCORRETA (FALSE).
-    printf("\n");
-    for(int i = 0; i < 10; i++)
-    {
-        BOOL ret = terminatecommandlexicalanalyser(terminate_syntaticanalyser_invalid_testcases[i]);
-        printf("%s: Valor esperado: FALSE. Valor retornado: %s. Analisador sintático 'terminate' para a sentença '%s'.\n",
-               ret == TRUE ? "FAILED" : "PASSED",
-               ret == TRUE ? "TRUE" : "FALSE",
-               terminate_syntaticanalyser_invalid_testcases[i]);
-        if(ret)
-            failed++;
-        else
-            passed++;
-        testcounter++;
-    }
-
-    // Testa cenários onde o comando 'addcommandlexicalanalyser' deve
-    // com sucesso criar um novo indivíduo.
-    printf("\n");
-    for(int i = 0; i < 12; i++)
-    {
-        stIndividuo individuo = { .paramId = "", .firstName = "", .lastName = "", .birthday = "", .phone = ""};
-        addcommandlexicalanalyser(add_syntaticanalyser_valid_testcases[i], &individuo);
-        addcommand(&individuo);
-    }
-
-    printf("\n\nTestes realizados: %d. (PASSED: %d, FAILED: %d)\n", testcounter, passed, failed);
+        case 0:// Testa cenário válido para o analisador léxico 'addcommandlexicalanalyser'
+        {
+            BOOL ret = addcommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: TRUE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 1:// Testa cenário válido para o analisador léxico 'delcommandlexicalanalyser'
+        {
+            BOOL ret = delcommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: TRUE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 2:// Testa cenário válido para o analisador léxico 'infocommandlexicalanalyser'
+        {
+            BOOL ret = infocommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: TRUE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 3:// Testa cenário válido para o analisador léxico 'querycommandlexicalanalyser'
+        {
+            BOOL ret = querycommandlexicalanalyser(sentence, &qry);
+            printf("Valor esperado: TRUE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 4:// Testa cenário válido para o analisador léxico 'terminatecommandlexicalanalyser'
+        {
+            BOOL ret = terminatecommandlexicalanalyser(sentence);
+            printf("Valor esperado: TRUE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 5:// Testa cenário INválido para o analisador léxico 'addcommandlexicalanalyser'
+        {
+            BOOL ret = addcommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: FALSE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 6:// Testa cenário INválido para o analisador léxico 'delcommandlexicalanalyser'
+        {
+            BOOL ret = delcommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: FALSE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 7:// Testa cenário INválido para o analisador léxico 'infocommandlexicalanalyser'
+        {
+            BOOL ret = infocommandlexicalanalyser(sentence, &individuo);
+            printf("Valor esperado: FALSE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 8:// Testa cenário INválido para o analisador léxico 'querycommandlexicalanalyser'
+        {
+            BOOL ret = querycommandlexicalanalyser(sentence, &qry);
+            printf("Valor esperado: FALSE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        case 9:// Testa cenário INválido para o analisador léxico 'terminatecommandlexicalanalyser'
+        {
+            BOOL ret = terminatecommandlexicalanalyser(sentence);
+            printf("Valor esperado: FALSE. Valor retornado: %s.",
+                   ret == TRUE ? "TRUE" : "FALSE");
+            break;
+        }
+        default:
+            break;
+    };
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    test_unittests_desafio_1();
+    test_unittests_desafio_1(atoi(argv[1]), argv[2]);
 
     return 0;
 }
